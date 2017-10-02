10b152aa032c0a7b8841d249d629e33485d89ec9
AnthonyJRamirez <jjorianst32@gmail.com>
2017-10-02 02:38:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ else {
   console.log("Expected the name to be Susan")
 }
 
+if (height === "70")  {
+    console.log("The height is correct")
+}
+else {
+  console.log("Expected to be taller")
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
